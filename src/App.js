28006b5b10cbca71da7fcf7ade3a5f87b0cba8b4
Michael Zavattaro <mwzavattaro@gmail.com>
2020-02-06 14:43:32 +1100

    updated layout with css grid

--- src/App.js
+++ src/App.js
@@ -6,10 +6,21 @@ import './app.css';
 // The WeekContainer component is being inserted into the App() fuction
 function App() {
     return (
-        <div className="app">
-            <div className="app-container">
+
+        <div className="lt-grid-container">
+            <div className="lt-header">
+                <h1>
+                    Weather Widget
+                </h1>
+            </div>
+            <div className="lt-widget-container">
                 <WeekContainer />
             </div>
+            <div className="lt-footer">
+                <span>Made with ❤️ for Koala 🐨</span>
+            </div>
+            <div className="lt-left-border" />
+            <div className="lt-right-border" />
         </div>
     );
 }
