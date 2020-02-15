6ab9aa2b63d7f301709f0e4dde496170308a6a50
Michael Zavattaro <mwzavattaro@gmail.com>
2020-02-15 12:09:06 +1100

    updated website copy

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ function App() {
         <div className="container">
             <div className="cell cell-1">
                 <h1>
-                    Weather Widget
+                    Weather Widget v2
                 </h1>
             </div>
             <div className="cell cell-2" />
@@ -19,7 +19,7 @@ function App() {
             </div>
             <div className="cell cell-4" />
             <div className="cell cell-5">
-                Made by Michael Zavattaro for the Koala Technical Test
+                Made by Michael Zavattaro
             </div>
         </div>
 
